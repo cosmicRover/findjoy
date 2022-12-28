aea4e0fce47bba84c738b96b8d9f8e01edcc1c92
Joy Paul <41096232+cosmicRover@users.noreply.github.com>
2022-12-27 21:23:31 -0500

    ypdate resume link

--- link_constants/constants.js
+++ link_constants/constants.js
@@ -1,6 +1,6 @@
 const constants = {
   resume:
-    "https://drive.google.com/file/d/1Wpxo0CR2qxBP2n6uhlPO0sMngSlON_zy/view?usp=sharing",
+    "https://drive.google.com/file/d/1H0qRMOvko_-pMxZM3rjRBvOnR1ZWUxJX/view?usp=share_link",
 
   in: "https://www.linkedin.com/in/joy-paul-050586b1/",
   git: "https://github.com/cosmicRover",
